a5e8cc32c4d2eba3ace5f56c8883e1a4dac3aade
Jochen Euler <je@cfus.de>
2016-06-08 00:27:00 +0200

    mod

--- uku2tex.rb
+++ uku2tex.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # uku2tex.rb
 # converts .uku files to latex for pdf generation
 
@@ -10,43 +11,8 @@ require 'rqrcode_png'
 $mode = 'inline'
 
 
-song='Titel: Buecken verboten
-Originaltitel: Küssen verboten!
-Band: Die Prinzen
-Barde: Jago
-Copyrighted: N
-Youtube: https://www.youtube.com/watch?v=sX-kH7KeAwU
-Tags: Dilli Chords
+song=''
 
-Willst mich _G_befördern, hast auch _C_schon einen stehn
-Ich muß _G_sagen, das kann ich _D_gut verstehn
-Du braust mir _G_Tränke und stehst _C_Wache für mich
-Bei´n _G_Antare, hab ich´s _D_gut erwischt
-Doch da _C_gibt es eine Sache, die ich _G_gar nicht leiden kann
-Kommen _C_deine beiden Backen zu _D_nah an mich ran...
-
-Ref.: Bücken ver_G_boten, bücken ver_D_boten
-Bücken ver_G_boten, streng ver_A_boten,
-_G_Keiner der Blau-_D_Weiß hier trägt
-Der _Em_hat mir das geglaubt : _D_Bücken ist bei mir nicht er_G_laubt!
-
-Schon als Rekrut, ja, ich war siebzehn Jahre alt
-Da war ein Priester in mich total verknallt
-Ging ich in´ Schlaufraum kam er hinter mir her
-Und in der Dusche wollte er noch viel mehr
-Er dachte, daß er mich mit  ´ner Beförd´rung kaufen kann
-Er bückte sich und bot mir seine Backen an....'
-
-
-
-template='
-\begin{multicols*}{2}
-\begin{song}{title}{}
-
-\end{song}
-\end{multicols*}
-
-'
 $title = nil
 $original = nil
 $band = nil
@@ -76,7 +42,10 @@ end
 
 
 
-
+# extracts the header information to global variables $title etc.
+# puts the rest after the header into $songtext
+# Sooner or later I'll start differentiating between verses and might even treat
+# the choruses differently :)
 def extract_data(song)
   # Songs have to start with the header, so the next is true
   in_header = true
@@ -84,7 +53,6 @@ def extract_data(song)
   song.split("\n").each do |line|
     # Ignore comments. Never hurts to have comments in a file format
     next if line.start_with? "#"
-
 
     # First, evaluate the header
     if in_header
@@ -104,14 +72,11 @@ def extract_data(song)
       # Skip parameters
       next if line[/^(Titel: |Originaltitel: |Band: |Barde: |Copyrighted: |Youtube: |Tags: )/]
 
-    else
+    else # Not in header
       # puts "Leerzeile" if  line[/^\s*$/]
-      # Add \\ for line breaks. We're not trying to create a diploma thesis here
-      $songtext += line.chomp + '\\\\' + "\n"
+      $songtext += line.chomp + "\n"
       # Lines increase :)
       $lines += 1
-
-      # Remove
       linelength = real_linelength(line)
       $width = linelength if linelength > $width
     end
@@ -119,34 +84,57 @@ def extract_data(song)
 end
 
 
+# Convert the uku chords into something usable for latex and our ukusongbook stylesheet
 def convert_chords(song)
-  $songtext.gsub(/_(.+?)_/, '\C{\1}')
+  song.gsub(/_(.+?)_/, '\C{\1}')
   # $songtext.gsub(/_(.+?)_/, '\hc{\1}')
 end
 
+
+# Create a QR code $title_qr.png  if the youtube info is set
 def write_youtube_qr
   if $youtube
     qr = RQRCode::QRCode.new( 'my string to generate', :size => 4, :level => :h )
     png = qr.to_img                                             # returns an instance of ChunkyPNG
     png.resize(90, 90).save("#{$title}_qr.png")
-
-    puts "wrote #{$title}_qr.png"
-  else
-    puts 'No Youtube link to write'
   end
 end
 
 
-def analyze_song (song)
-
-
-end
-
-
-
+# Add linebreaks, header and footer
 def latexify (song)
+  songtext = '\begin{ukusong}{%s}
+' % [ $title ]
 
+  song.split("\n").each do |line|
+     
+      songtext += line + '\\\\' +  "\n"
+  end
+  
+  songtext + '\end{ukusong}'
+	
 end
+
+
+
+if ARGV.empty?
+  puts "Usage: #{$0} <file to convert  from uku to tex\n"
+  exit 1
+end
+
+file = ARGV[0]
+
+if File.exists? file
+  song = File.read file 
+else
+  puts "Can't read #{file}."
+  exit 1
+end
+
+
+
+
+
 
 
 
@@ -154,16 +142,14 @@ end
 
 # Separate header information
 extract_data song
-puts "#{$lines} Lines, broadest line is #{$width}"
-
 
 # move chords from _C_ to \C{C}
-song = convert_chords song
+song = convert_chords $songtext
+
 write_youtube_qr
 
-# Embed song and metadata into includable Latex
 
-
+puts latexify song
 
 
 
